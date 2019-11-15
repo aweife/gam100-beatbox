0ad785d2cab34d8477a4c854cca19ad524bed936
Ted <weifeng.ang@digipen.edu>
2019-11-15 12:27:15 +0800

    Increase console size

--- RhythmGame/RhythmGame/Console/Console.c
+++ RhythmGame/RhythmGame/Console/Console.c
@@ -159,8 +159,8 @@ void Console_SetSquareFont()
 	CONSOLE_FONT_INFOEX fontInfo = { 0 };
 	fontInfo.cbSize = sizeof(fontInfo);
 	wcscpy_s(fontInfo.FaceName, _countof(fontInfo.FaceName), L"Terminal");
-	fontInfo.dwFontSize.X = 8;
-	fontInfo.dwFontSize.Y = 8;
+	fontInfo.dwFontSize.X = 1;
+	fontInfo.dwFontSize.Y = 1;
 	fontInfo.FontWeight = FW_NORMAL;
 	sConsoleDisplayRatio = fontInfo.dwFontSize.X / (float)fontInfo.dwFontSize.Y;
 	
--- RhythmGame/RhythmGame/Global.h
+++ RhythmGame/RhythmGame/Global.h
@@ -2,8 +2,8 @@
 
 #include <Windows.h>
 
-#define GAME_WIDTH 100
-#define GAME_HEIGHT 80
+#define GAME_WIDTH 400
+#define GAME_HEIGHT 200
 
 typedef enum CONSOLECOLOR {
 	BLACK = 0,
