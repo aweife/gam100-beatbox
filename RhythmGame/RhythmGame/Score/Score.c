b48a737bf7f007fac97b195bb02ff01d701d76ea
fikrulsmith <56424891+fikrulsmith@users.noreply.github.com>
2019-11-19 17:11:35 +0800

    Implemented Scoring System
    Struct not fully developed yet but the functions are able to write and read the score.

--- RhythmGame/RhythmGame/Score/Score.c
+++ RhythmGame/RhythmGame/Score/Score.c
@@ -1,21 +1,66 @@
 #define _CRT_SECURE_NO_DEPRECATE
 
 #include "Score.h"
-#include <stdio.h>
-#include "../Player/Player.h"
-#include "../Clock/Clock.h"
 
-FILE* scoreboardFile;
-char scoreDisplay[10];
+FILE* writeScoreFile;
+FILE* readScoreFile;
 
-void _RenderScoreBoard(int score)
+static char scoreDisplay[256];
+static char buffer[BUFFER_SIZE];
+
+Player temp;
+Player p[SIZE];
+
+//Internal Functions
+
+void _RenderScoreBoard(int score, int x, int y)
 {
 	_itoa_s(score, scoreDisplay, sizeof(scoreDisplay), 10);
-	Console_SetRenderBuffer_String(53, 10, "Score:");
-	Console_SetRenderBuffer_String(60, 10, scoreDisplay);
+	Console_SetRenderBuffer_String(x, y, "Score:");
+	Console_SetRenderBuffer_String(x + 7, y, scoreDisplay);
 }
 
-//write to file
-scoreboardFile = fopen("..//RhythmGame//$Resources//Score.txt", "r");
+void _InputScore(char* path)
+{
+	//Write To Score File
+	writeScoreFile = fopen(path, "a");
+	if (writeScoreFile == NULL)
+	{
+		perror("Error opening file");
+	}
+	else {
+		fprintf(writeScoreFile, "Player Score: %d\n", Get_Score());
+		fclose(writeScoreFile);
+	}
+}
 
+void _OutputScore(char* path, int x, int y)
+{
+	//Read from Score File
+	readScoreFile = fopen(path, "rt");
+	if (readScoreFile == NULL)
+	{
+		perror("Error opening file");
+	}
+	else {
+		while (!feof(writeScoreFile))
+		{
+			if (fgets(buffer, BUFFER_SIZE, writeScoreFile))
+			{
 
+				for (int i = 0; i < BUFFER_SIZE; i++)
+				{
+					if (buffer[i] == '\n')
+						buffer[i] = ' ';
+
+				}
+
+				Console_SetRenderBuffer_String(x, y, buffer);
+				y += 1;
+				//printf(buffer);
+			}
+
+		}
+		fclose(writeScoreFile);
+	}
+}
--- RhythmGame/RhythmGame/Score/Score.h
+++ RhythmGame/RhythmGame/Score/Score.h
@@ -1,3 +1,19 @@
 #pragma once
 
-extern void _RenderScoreBoard(int score);
+#include <stdbool.h>
+#include "../States/Game.h"
+#include <stdio.h>
+#include "../Clock/Clock.h"
+#include "../Console/Console.h"
+
+#define BUFFER_SIZE 100
+#define SIZE 30
+
+typedef struct {
+	char name[20];
+	int score;
+} Player;
+
+extern void _RenderScoreBoard(int score, int x, int y);
+extern void _InputScore(char* path);
+extern void _OutputScore(char* path, int x, int y);
