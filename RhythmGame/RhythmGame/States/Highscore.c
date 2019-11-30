00fcfbc6489e9c6e45d574130cfdfdcbcaaa713a
ghthegreat <56424881+ghthegreat@users.noreply.github.com>
2019-11-30 10:39:42 +0800

    Added highscore files

---
+++ RhythmGame/RhythmGame/States/Highscore.c
@@ -0,0 +1,31 @@
+#include "MainMenu.h"
+#include <Windows.h>
+#include "../Text/TextReader.h"
+#include "StateMachine.h"
+#include "Game.h"
+#include"../UI/UI.h"
+
+void Highscore_ProcessInput()
+{
+
+}
+
+void Highscore_Update()
+{
+
+}
+
+void Highscore_Render()
+{
+
+}
+
+void Highscore_Enterstate()
+{
+
+}
+
+void Highscore_Exitstate()
+{
+
+}
