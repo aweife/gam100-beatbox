060a00312d199e2a2d6af56c5e6f6ec5ef9ed376
ghthegreat <56424881+ghthegreat@users.noreply.github.com>
2019-11-30 17:22:53 +0800

    testing

--- RhythmGame/RhythmGame/States/Highscore.c
+++ RhythmGame/RhythmGame/States/Highscore.c
@@ -6,17 +6,20 @@
 #include"../UI/UI.h"
 
 sprite HS;
-sprite trophy;
+sprite trophyman;
+sprite trophyman2;
+
+void ReadfromTextHS();
 
 void Highscore_EnterState()
 {
 	HS = Text_CreateSprite();
-	Text_Init(&HS, "..//RhythmGame//$Resources//Highscore.txt");
-	Text_Move(&HS,5, 10);
+	Text_Init(&HS, "..//RhythmGame//$Resources//Highscore2.txt");
+	Text_Move(&HS,5, 5);
 
-	trophy = Text_CreateSprite();
-	Text_Init(&trophy, "..//RhythmGame//$Resources//trophy2.txt");
-	Text_Move(&trophy,10,25);
+	trophyman = Text_CreateSprite();
+	Text_Init(&trophyman, "..//RhythmGame//$Resources//trophyman3.txt");
+	Text_Move(&trophyman,10,25);
 }
 
 void Highscore_ExitState()
@@ -37,6 +40,11 @@ void Highscore_Update()
 void Highscore_Render()
 {
 	Text_Render(&HS,0,0);
-	Text_Render(&trophy, 0, 0);
+	Text_Render(&trophyman, 0, 0);
 }
 
+void _ReadfromTextHS()
+{
+
+
+}
